659dec54bf17d68ba882dedd085296d0e8deaf55
Alexander Rebello <alexander@rebello.eu>
2021-12-28 09:15:11 +0100

    Update Styling

--- index.php
+++ index.php
@@ -69,55 +69,136 @@ if( !$title ) { $title = clean_title(basename(dirname(__FILE__))); }
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
 	<link href="//fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet" type="text/css" />
 	<style>
-		*, *:before, *:after { -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; }
-		body { background: #dadada; font-family: "Lato", "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; font-weight: 400; font-size: 14px; line-height: 18px; padding: 0; margin: 0; text-align: center;}
-		.wrap { max-width: 100%; width: 500px; margin: 20px auto; background: white; padding: 40px; border-radius: 3px; text-align: left;}
-		@media only screen and (max-width: 700px) { .wrap { padding: 15px; } }
-		h1 { text-align: center; margin: 40px 0; font-size: 22px; font-weight: bold; color: #666; }
-		a { color: #399ae5; text-decoration: none; } a:hover { color: #206ba4; text-decoration: none; }
-		.note { padding:  0 5px 25px 0; font-size:80%; color: #666; line-height: 18px; }
-		.block { clear: both; min-height: 50px; border-top: solid 1px #ECE9E9; }
-		.block:first-child { border: none; }
-		.block .img { width: 50px; height: 50px; display: block; float: left; margin-right: 10px; background: transparent url(<?php echo $icon_url; ?>) no-repeat 0 0; }
-		.block .file { padding-bottom: 5px; }
-		.block .data { line-height: 1.3em; color: #666; }
-		.block a { display: block; padding: 20px; transition: all 0.35s; }
-		.block a:hover, .block a.open { text-decoration: none; background: #efefef; }
-		
-		.bold { font-weight: 900; }
-		.upper { text-transform: uppercase; }
-		.fs-1 { font-size: 1em; } .fs-1-1 { font-size: 1.1em; } .fs-1-2 { font-size: 1.2em; } .fs-1-3 { font-size: 1.3em; } .fs-0-9 { font-size: 0.9em; } .fs-0-8 { font-size: 0.8em; } .fs-0-7 { font-size: 0.7em; }
-		
-		.jpg, .jpeg, .gif, .png { background-position: -50px 0 !important; } 
-		.pdf { background-position: -100px 0 !important; }  
-		.txt, .rtf { background-position: -150px 0 !important; }
-		.xls, .xlsx { background-position: -200px 0 !important; } 
-		.ppt, .pptx { background-position: -250px 0 !important; } 
-		.doc, .docx { background-position: -300px 0 !important; }
-		.zip, .rar, .tar, .gzip { background-position: -350px 0 !important; }
-		.swf { background-position: -400px 0 !important; } 
-		.fla { background-position: -450px 0 !important; }
-		.mp3 { background-position: -500px 0 !important; }
-		.wav { background-position: -550px 0 !important; }
-		.mp4 { background-position: -600px 0 !important; }
-		.mov, .aiff, .m2v, .avi, .pict, .qif { background-position: -650px 0 !important; }
-		.wmv, .avi, .mpg { background-position: -700px 0 !important; }
-		.flv, .f2v { background-position: -750px 0 !important; }
-		.psd { background-position: -800px 0 !important; }
-		.ai { background-position: -850px 0 !important; }
-		.html, .xhtml, .dhtml, .php, .asp, .css, .js, .inc { background-position: -900px 0 !important; }
-		.dir { background-position: -950px 0 !important; }
-		
-		.sub { margin-left: 20px; border-left: solid 5px #ECE9E9; display: none; }
-		
-		body.dark { background: #1d1c1c; color: #fff; }
-		body.dark h1 { color: #fff; }
-		body.dark .wrap { background: #2b2a2a; }
-		body.dark .block { border-top: solid 1px #666; }
-		body.dark .block a:hover, body.dark .block a.open { background: #000; }
-		body.dark .note { color: #fff; }
-		body.dark .block .data { color: #fff; }
-		body.dark .sub { border-left: solid 5px #0e0e0e; }
+		*,
+		*:before,
+		*:after {
+		    -moz-box-sizing: border-box;
+		    -webkit-box-sizing: border-box;
+		    box-sizing: border-box;
+		}
+
+		body {
+		    background: #1d1c1c;
+		    font-family: "Lato", "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+		    font-weight: 400;
+		    font-size: 14px;
+		    line-height: 18px;
+		    padding: 0;
+		    margin: 0;
+		    text-align: center;
+		    color: #fff;
+		}
+
+		.wrap {
+		    max-width: 100%;
+		    width: 500px;
+		    margin: 20px auto;
+		    background: #2b2a2a;
+		    padding: 40px;
+		    border-radius: 3px;
+		    text-align: left;
+		}
+
+		@media only screen and (max-width: 700px) {
+		    .wrap {
+			padding: 15px;
+		    }
+		}
+
+		h1 {
+		    text-align: center;
+		    margin: 40px 0;
+		    font-size: 22px;
+		    font-weight: bold;
+		    color: #fff;
+		}
+
+		a {
+		    color: #399ae5;
+		    text-decoration: none;
+		}
+
+		a:hover {
+		    color: #206ba4;
+		    text-decoration: none;
+		}
+
+		.note {
+		    padding: 0 5px 25px 0;
+		    font-size: 80%;
+		    line-height: 18px;
+		    color: #fff;
+		}
+
+		.block {
+		    clear: both;
+		    min-height: 50px;
+		    border-top: solid 1px #666;
+				height: 95px;
+		}
+
+		.block:first-child {
+		    border: none;
+		}
+
+		.block .icon {
+				width: 50px;
+				height: 55px;
+				display: block;
+				float: left;
+				margin-right: 10px;
+				font-size: 40px;
+				line-height: 55px;
+				text-align: center;
+			}
+
+		.block .file {
+		    padding-bottom: 5px;
+		}
+
+		body .block .data {
+		    line-height: 1.3em;
+		    color: #fff;
+		}
+
+		.block a {
+		    display: block;
+		    padding: 20px;
+		    transition: all 0.35s;
+				height: 95px
+		}
+
+		body .block a:hover,
+		body .block a.open {
+		    text-decoration: none;
+		    background: rgba(255, 255, 255, .05);
+		}
+
+		.bold {
+		    font-weight: 900;
+		}
+
+		.upper {
+		    text-transform: uppercase;
+		}
+
+		.fs-1-2 {
+		    font-size: 1.2em;
+		}
+
+		.fs-0-8 {
+		    font-size: 0.8em;
+		}
+
+		.fs-0-7 {
+		    font-size: 0.7em;
+		}
+
+		.sub {
+		    margin-left: 20px;
+		    display: none;
+		    border-left: solid 5px #666;
+		}
 	</style>
 </head>
 <body class="<?php echo $color; ?>">
