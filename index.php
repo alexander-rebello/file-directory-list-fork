624e4f0f10156fa4cf098a1df0492222470851b4
Alexander Rebello <alexander@rebello.eu>
2021-12-28 09:19:47 +0100

    Update scripts

--- index.php
+++ index.php
@@ -176,205 +176,196 @@ $settings = array(
 		}
 	</style>
 </head>
-<body class="<?php echo $color; ?>">
-<h1><?php echo $title ?></h1>
-<div class="wrap">
-<?php
-
-// FUNCTIONS TO MAKE THE MAGIC HAPPEN, BEST TO LEAVE THESE ALONE
-function clean_title($title)
-{
-	return ucwords( str_replace( array("-", "_"), " ", $title) );
-}
-
-function ext($filename) 
-{
-	return substr( strrchr( $filename,'.' ),1 );
-}
-
-function display_size($bytes, $precision = 2) 
-{
-	$units = array('B', 'KB', 'MB', 'GB', 'TB');
-    $bytes = max($bytes, 0); 
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
-    $pow = min($pow, count($units) - 1); 
-    $bytes /= (1 << (10 * $pow)); 
-	return round($bytes, $precision) . '<span class="fs-0-8 bold">' . $units[$pow] . "</span>";
-}
-
-function count_dir_files( $dir)
-{
-	$fi = new FilesystemIterator(__DIR__ . "/" . $dir, FilesystemIterator::SKIP_DOTS);
-	return iterator_count($fi);
-}
-
-function get_directory_size($path)
-{
-    $bytestotal = 0;
-    $path = realpath($path);
-    if($path!==false && $path!='' && file_exists($path))
-    {
-        foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS)) as $object)
+<body>
+    <h1><?php echo $settings["title"] ?></h1>
+    <div class="wrap">
+        <?php
+        function ext($filename)
         {
-            $bytestotal += $object->getSize();
+            return substr(strrchr($filename, '.'), 1);
         }
-    }
-    
-    return display_size($bytestotal);
-}
+
+        function display_size($bytes, $precision = 2)
+        {
+            $units = array('B', 'KB', 'MB', 'GB', 'TB');
+            $bytes = max($bytes, 0);
+            $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+            $pow = min($pow, count($units) - 1);
+            $bytes /= (1 << (10 * $pow));
+            return round($bytes, $precision) . '<span class="fs-0-8 bold">' . $units[$pow] . "</span>";
+        }
+
+        function count_dir_files($dir)
+        {
+            $fi = new FilesystemIterator(__DIR__ . "/" . $dir, FilesystemIterator::SKIP_DOTS);
+            return iterator_count($fi);
+        }
+
+        function get_directory_size($path)
+        {
+            $bytestotal = 0;
+            $path = realpath($path);
+            if ($path !== false && $path != '' && file_exists($path)) {
+                foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS)) as $object) {
+                    $bytestotal += $object->getSize();
+                }
+            }
+
+            return display_size($bytestotal);
+        }
 
 
-// SHOW THE MEDIA BLOCK
-function display_block( $file )
-{
-	global $ignore_file_list, $ignore_ext_list, $force_download;
-	
-	$file_ext = ext($file);
-	if( !$file_ext AND is_dir($file)) $file_ext = "dir";
-	if(in_array($file, $ignore_file_list)) return;
-	if(in_array($file_ext, $ignore_ext_list)) return;
-	
-	$download_att = ($force_download AND $file_ext != "dir" ) ? " download='" . basename($file) . "'" : "";
-	
-	$rtn = "<div class=\"block\">";
-	$rtn .= "<a href=\"$file\" class=\"$file_ext\"{$download_att}>";
-	$rtn .= "	<div class=\"img $file_ext\"></div>";
-	$rtn .= "	<div class=\"name\">";
-	
-	if ($file_ext === "dir") 
-	{
-		$rtn .= "		<div class=\"file fs-1-2 bold\">" . basename($file) . "</div>";
-		$rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">" . count_dir_files($file) . "</span> files</div>";
-		$rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">Size:</span> " . get_directory_size($file) . "</div>";
-		
-	}
-	else
-	{
-		$rtn .= "		<div class=\"file fs-1-2 bold\">" . basename($file) . "</div>";
-		$rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">Size:</span> " . display_size(filesize($file)) . "</div>";
-		$rtn .= "		<div class=\"data upper modified fs-0-7\"><span class=\"bold\">Last modified:</span> " .  date("D. F jS, Y - h:ia", filemtime($file)) . "</div>";	
-	}
-
-	$rtn .= "	</div>";
-	$rtn .= "	</a>";
-	$rtn .= "</div>";
-	return $rtn;
-}
-
-
-// RECURSIVE FUNCTION TO BUILD THE BLOCKS
-function build_blocks( $items, $folder )
-{
-	global $ignore_file_list, $ignore_ext_list, $sort_by, $toggle_sub_folders, $ignore_empty_folders;
-	
-	$objects = array();
-	$objects['directories'] = array();
-	$objects['files'] = array();
-	
-	foreach($items as $c => $item)
-	{
-		if( $item == ".." OR $item == ".") continue;
-	
-		// IGNORE FILE
-		if(in_array($item, $ignore_file_list)) { continue; }
-	
-		if( $folder && $item )
-		{
-			$item = "$folder/$item";
-		}
-
-		$file_ext = ext($item);
-		
-		// IGNORE EXT
-		if(in_array($file_ext, $ignore_ext_list)) { continue; }
-		
-		// DIRECTORIES
-		if( is_dir($item) ) 
-		{
-			$objects['directories'][] = $item; 
-			continue;
-		}
-		
-		// FILE DATE
-		$file_time = date("U", filemtime($item));
-		
-		// FILES
-		if( $item )
-		{
-			$objects['files'][$file_time . "-" . $item] = $item;
-		}
-	}
-	
-	foreach($objects['directories'] as $c => $file)
-	{
-		$sub_items = (array) scandir( $file );
-		
-		if( $ignore_empty_folders )
-		{
-			$has_sub_items = false;
-			foreach( $sub_items as $sub_item )
-			{
-				$sub_fileExt = ext( $sub_item );
-				if( $sub_item == ".." OR $sub_item == ".") continue;
-				if(in_array($sub_item, $ignore_file_list)) continue;
-				if(in_array($sub_fileExt, $ignore_ext_list)) continue;
+        // SHOW THE MEDIA BLOCK
+        function display_block($file)
+        {
+            global $settings;
 			
-				$has_sub_items = true;
-				break;	
-			}
-			
-			if( $has_sub_items ) echo display_block( $file );
-		}
-		else
-		{
-			echo display_block( $file );
-		}
-		
-		if( $toggle_sub_folders )
-		{
-			if( $sub_items )
-			{
-				echo "<div class='sub' data-folder=\"$file\">";
-				build_blocks( $sub_items, $file );
-				echo "</div>";
-			}
-		}
-	}
-	
-	// SORT BEFORE LOOP
-	if( $sort_by == "date_asc" ) { ksort($objects['files']); }
-	elseif( $sort_by == "date_desc" ) { krsort($objects['files']); }
-	elseif( $sort_by == "name_asc" ) { natsort($objects['files']); }
-	elseif( $sort_by == "name_desc" ) { arsort($objects['files']); }
-	
-	foreach($objects['files'] as $t => $file)
-	{
-		$fileExt = ext($file);
-		if(in_array($file, $ignore_file_list)) { continue; }
-		if(in_array($fileExt, $ignore_ext_list)) { continue; }
-		echo display_block( $file );
-	}
-}
+			$icons = [
+				"html" => "fa-brands fa-html5",
+				"php" => "fa-brands fa-php",
+				"css" => "fa-brands fa-css",
+				"js" => "fa-brands fa-js",
+				"python" => "fa-brands fa-python",
+				"jpg" => "fa-solid fa-file-image",
+				"png" => $icons["jpg"],
+				"jpeg" => $icons["jpg"],
+				"gif" => $icons["jpg"],
+				"pdf" => "fa-solid fa-file-pdf",
+				"txt" => "fa-solid fa-file-lines",
+				"rtf" => $icons["txt"],
+				"zip" => "fa-solid fa-sile-zipper",
+				"rar" => $icons["zip"],
+				"tar" => $icons["zip"],
+				"gzip" => $icons["zip"],
+				"dir" => "fa-solid fa-folder",
+				"file" => "fa-solid fa-file-code",
+			];
 
-// GET THE BLOCKS STARTED, FALSE TO INDICATE MAIN FOLDER
-$items = scandir( dirname(__FILE__) );
-build_blocks( $items, false );
-?>
+            $file_ext = ext($file);
+            if (!$file_ext and is_dir($file)) $file_ext = "dir";
+			if (!array_key_exists($file_ext,$icons)) $file_ext = "file";
+            if (in_array($file, $settings["ignore_file_list"])) return;
+            if (in_array($file_ext, $settings["ignore_ext_list"])) return;
 
-<?php if($toggle_sub_folders) { ?>
-<script type="text/javascript">
-	$(document).ready(function() 
-	{
-		$("a.dir").click(function(e)
-		{
-			$(this).toggleClass('open');
-		 	$('.sub[data-folder="' + $(this).attr('href') + '"]').slideToggle();
-			e.preventDefault();
-		});
-	});
-</script>
-<?php } ?>
-</div>
-<div style="padding: 10px 10px 40px 10px;"><a href="https://halgatewood.com/free/file-directory-list/">Free PHP File Directory Script</a> (<a href="https://github.com/halgatewood/file-directory-list/">GitHub</a>)</div>
+            $rtn = "<div class=\"block\">";
+            $rtn .= "<a href=\"$file\" class=\"$file_ext\">";
+            $rtn .= "	<i class=\"icon {$icons[$file_ext]}\"></i>";
+            $rtn .= "	<div class=\"name\">";
+
+            if ($file_ext === "dir") {
+                $rtn .= "		<div class=\"file fs-1-2 bold\">" . basename($file) . "</div>";
+                $rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">" . count_dir_files($file) . "</span> files</div>";
+                $rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">Size:</span> " . get_directory_size($file) . "</div>";
+            } else {
+                $rtn .= "		<div class=\"file fs-1-2 bold\">" . basename($file) . "</div>";
+                $rtn .= "		<div class=\"data upper size fs-0-7\"><span class=\"bold\">Size:</span> " . display_size(filesize($file)) . "</div>";
+                $rtn .= "		<div class=\"data upper modified fs-0-7\"><span class=\"bold\">Last modified:</span> " .  date("D. F jS, Y - h:ia", filemtime($file)) . "</div>";
+            }
+
+            $rtn .= "	</div>";
+            $rtn .= "	</a>";
+            $rtn .= "</div>";
+            return $rtn;
+        }
+
+
+        // RECURSIVE FUNCTION TO BUILD THE BLOCKS
+        function build_blocks($items, $folder)
+        {
+            global $settings;
+
+            $objects = array();
+            $objects['directories'] = array();
+            $objects['files'] = array();
+
+            foreach ($items as $c => $item) {
+                if ($item == ".." or $item == ".") continue;
+
+                // IGNORE FILE
+                if (in_array($item, $settings["ignore_file_list"])) {
+                    continue;
+                }
+
+                if ($folder && $item) {
+                    $item = "$folder/$item";
+                }
+
+                $file_ext = ext($item);
+
+                // IGNORE EXT
+                if (in_array($file_ext, $settings["ignore_ext_list"])) {
+                    continue;
+                }
+
+                // DIRECTORIES
+                if (is_dir($item)) {
+                    $objects['directories'][] = $item;
+                    continue;
+                }
+
+                // FILE DATE
+                $file_time = date("U", filemtime($item));
+
+                // FILES
+                if ($item) {
+                    $objects['files'][$file_time . "-" . $item] = $item;
+                }
+            }
+
+            foreach ($objects['directories'] as $c => $file) {
+                $sub_items = (array) scandir($file);
+
+                $has_sub_items = false;
+                foreach ($sub_items as $sub_item) {
+                    $sub_fileExt = ext($sub_item);
+                    if ($sub_item == ".." or $sub_item == ".") continue;
+                    if (in_array($sub_item, $settings["ignore_file_list"])) continue;
+                    if (in_array($sub_fileExt, $settings["ignore_ext_list"])) continue;
+
+                    $has_sub_items = true;
+                    break;
+                }
+
+                if ($has_sub_items) echo display_block($file);
+
+                if ($sub_items) {
+                    echo "<div class='sub' data-folder=\"$file\">";
+                    build_blocks($sub_items, $file);
+                    echo "</div>";
+                }
+            }
+
+            arsort($objects['files']);
+
+            foreach ($objects['files'] as $t => $file) {
+                $fileExt = ext($file);
+                if (in_array($file, $settings["ignore_file_list"])) {
+                    continue;
+                }
+                if (in_array($fileExt, $settings["ignore_ext_list"])) {
+                    continue;
+                }
+                echo display_block($file);
+            }
+        }
+
+        // GET THE BLOCKS STARTED, FALSE TO INDICATE MAIN FOLDER
+        $items = scandir(dirname(__FILE__));
+        build_blocks($items, false);
+        ?>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $("a.dir").click(function(e) {
+                    $(this).toggleClass('open');
+                    $('.sub[data-folder="' + $(this).attr('href') + '"]').slideToggle();
+                    e.preventDefault();
+                });
+            });
+        </script>
+    </div>
+    <div style="padding: 10px 10px 40px 10px;">
+        <a href="https://github.com/halgatewood/file-directory-list/">Credit</a>
+    </div>
 </body>
 </html>
